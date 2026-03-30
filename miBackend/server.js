92bca051b980ed1359d668c6082e4669bf604bd6
Josthin <josthincitosss79@gmail.com>
2026-03-30 12:38:19 -0500

    Borre espacios

--- miBackend/server.js
+++ miBackend/server.js
@@ -3,28 +3,23 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 let db; // 👈 memoria temporal
-
 async function conectarDB() {
   await client.connect();
   db = client.db("miApp");
   console.log("Conectado a MongoDB 🔥");
 }
-
 const servidor = http.createServer((req, res) => {
   console.log("URL recibida:", req.url);
   
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
-  // 🟢 Ruta raíz
-  if (req.method === "GET" && req.url === "/") {
+  if (req.method === "GET" && req.url === "/") { //Ruta Raiz
     res.write("API funcionando 🔥");
     res.end();
     return;
   }
-  // 👉 cuando piden la lista GET usuario
-  if (req.method === "GET" && req.url.startsWith("/usuarios")) {
+  if (req.method === "GET" && req.url.startsWith("/usuarios")) { // 👉 cuando piden la lista GET usuario
     db.collection("usuarios").find().toArray()
 	.then(data => {
 		res.setHeader("Content-Type", "application/json");
@@ -33,18 +28,13 @@ const servidor = http.createServer((req, res) => {
 	});
 	return;
   }
-  
-  // 👉 cuando envían un nuevo usuario POST usuario
-  if (req.method === "POST" && req.url.startsWith("/usuarios")) {
+  if (req.method === "POST" && req.url.startsWith("/usuarios")) { // 👉 cuando envían un nuevo usuario POST usuario
     let body = "";
-
     req.on("data", chunk => {
       body += chunk;
     });
-
     req.on("end", () => {
       const nuevoUsuario = JSON.parse(body);
-
       db.collection("usuarios").insertOne({
 		  nombre: nuevoUsuario.nombre
 	  }).then(() => {
@@ -54,30 +44,21 @@ const servidor = http.createServer((req, res) => {
     });
 	return;
   }
-  
-  //Deletear usuarios
-  if (req.method === "DELETE" && req.url.startsWith("/usuarios/")) {
-
+  if (req.method === "DELETE" && req.url.startsWith("/usuarios/")) { //Deletear usuarios
   const id = req.url.split("/")[2].trim();
   console.log("ID recibido", id);
-
   const { ObjectId } = require("mongodb");
-
   db.collection("usuarios").deleteOne({ _id: new ObjectId(id) })
     .then(() => {
       res.write("Usuario eliminado 🔥");
       res.end();
     });
-
   return;
   }
-  
   res.write("Ruta no encontrada");
   res.end();
 });
-
 const PORT = process.env.PORT || 3000;
-
 conectarDB().then(() => {
   servidor.listen(PORT, "0.0.0.0", () => {
     console.log("Servidor corriendo 🔥");
